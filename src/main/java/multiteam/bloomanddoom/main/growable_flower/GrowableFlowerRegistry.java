b8382aa4c51cd1ca282a170fd46eeeb79b330c14
LazTheArtist <kingofcreepersyt@gmail.com>
2022-08-30 17:28:50 +0200

    i've made 3 flowers and i already have enough

--- src/main/java/multiteam/bloomanddoom/main/growable_flower/GrowableFlowerRegistry.java
+++ src/main/java/multiteam/bloomanddoom/main/growable_flower/GrowableFlowerRegistry.java
@@ -4,6 +4,7 @@ import com.mojang.math.Vector3d;
 import multiteam.bloomanddoom.main.Registration;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import oshi.util.tuples.Pair;
 
 import java.util.List;
@@ -13,13 +14,104 @@ public class GrowableFlowerRegistry {
     public static void register(){
         Registration.GROWABLE_FLOWERS.add(
                 new GrowableFlower(
+                        Blocks.DANDELION,
+                        List.of(
+                                block(Blocks.LIME_CONCRETE, 0d, 0d, 0d),
+                                block(Blocks.LIME_CONCRETE, 0d, 1d, 0d),
+                                block(Blocks.LIME_CONCRETE, 0d, 2d, 0d),
+                                block(Blocks.LIME_CONCRETE, -1d, 1d, 0d),
+                                block(Blocks.LIME_CONCRETE, -1d, 2d, -1d),
+                                block(Blocks.LIME_CONCRETE, 1d, 1d, 0d),
+                                block(Blocks.LIME_CONCRETE, 1d, 1d, 1d),
+                                block(Blocks.YELLOW_CONCRETE, 0d, 3d, 0d),
+                                block(Blocks.YELLOW_CONCRETE, 0d, 4d, 0d),
+                                block(Blocks.YELLOW_CONCRETE, -1d, 3d, 0d),
+                                block(Blocks.YELLOW_CONCRETE, -1d, 4d, 0d),
+                                block(Blocks.YELLOW_CONCRETE, 0d, 3d, 0d),
+                                block(Blocks.YELLOW_CONCRETE, 1d, 3d, 0d),
+                                block(Blocks.YELLOW_CONCRETE, 1d, 3d, 1d),
+                                block(Blocks.YELLOW_CONCRETE, 1d, 4d, 1d),
+                                block(Blocks.YELLOW_CONCRETE, 0d, 3d, 0d),
+                                block(Blocks.YELLOW_CONCRETE, 0d, 3d, -1d),
+                                block(Blocks.YELLOW_CONCRETE, 0d, 4d, -1d),
+                                block(Blocks.YELLOW_CONCRETE, 1d, 3d, -1d),
+                                block(Blocks.YELLOW_CONCRETE, 0d, 3d, 1d)
+                        )
+                )
+        );
+        Registration.GROWABLE_FLOWERS.add(
+                new GrowableFlower(
                         Blocks.POPPY,
                         List.of(
-                                new Pair<>(Blocks.GREEN_CONCRETE.defaultBlockState(), new Vector3d(0d, 0d, 0d)),
-                                new Pair<>(Blocks.GREEN_CONCRETE.defaultBlockState(), new Vector3d(0d, 1d, 0d)),
-                                new Pair<>(Blocks.GREEN_CONCRETE.defaultBlockState(), new Vector3d(0d, 2d, 0d)),
-                                new Pair<>(Blocks.GREEN_CONCRETE.defaultBlockState(), new Vector3d(0d, 3d, 0d)),
-                                new Pair<>(Blocks.RED_CONCRETE.defaultBlockState(), new Vector3d(0d, 4d, 0d))
+                                block(Blocks.GREEN_CONCRETE, 0d, 0d, 0d),
+                                block(Blocks.GREEN_CONCRETE, 0d, 1d, 0d),
+                                block(Blocks.GREEN_CONCRETE, 0d, 2d, 0d),
+                                block(Blocks.GREEN_CONCRETE, 0d, 3d, 0d),
+                                block(Blocks.GREEN_CONCRETE, 0d, 4d, 0d),
+                                block(Blocks.GREEN_CONCRETE, 1d, 0d, 0d),
+                                block(Blocks.GREEN_CONCRETE, -1d, 0d, 0d),
+                                block(Blocks.GREEN_CONCRETE, 0d, 0d, 1d),
+                                block(Blocks.GREEN_CONCRETE, 0d, 0d, -1d),
+                                block(Blocks.GREEN_CONCRETE, 2d, 1d, 0d),
+                                block(Blocks.GREEN_CONCRETE, -2d, 1d, 0d),
+                                block(Blocks.GREEN_CONCRETE, 0d, 1d, 2d),
+                                block(Blocks.GREEN_CONCRETE, 0d, 1d, -2d),
+                                block(Blocks.RED_CONCRETE, 0d, 4d, 1d),
+                                block(Blocks.RED_CONCRETE, 1d, 4d, 1d),
+                                block(Blocks.RED_CONCRETE, -1d, 4d, 0d),
+                                block(Blocks.RED_CONCRETE, -1d, 5d, 0d),
+                                block(Blocks.RED_CONCRETE, 0d, 5d, 0d),
+                                block(Blocks.RED_CONCRETE, -1d, 5d, 1d),
+                                block(Blocks.RED_CONCRETE, -1d, 4d, -1d),
+                                block(Blocks.RED_CONCRETE, -1d, 5d, -1d),
+                                block(Blocks.RED_CONCRETE, -1d, 5d, 1d),
+                                block(Blocks.RED_CONCRETE, 0d, 5d, 0d),
+                                block(Blocks.RED_CONCRETE, 0d, 4d, -1d),
+                                block(Blocks.RED_CONCRETE, 0d, 5d, -1d),
+                                block(Blocks.RED_CONCRETE, 1d, 5d, -1d),
+                                block(Blocks.RED_CONCRETE, 0d, 6d, 1d),
+                                block(Blocks.RED_CONCRETE, 0d, 6d, -1d),
+                                block(Blocks.RED_CONCRETE, -1d, 6d, 0d),
+                                block(Blocks.RED_CONCRETE, -1d, 6d, 1d),
+                                block(Blocks.RED_CONCRETE, 0d, 6d, 1d),
+                                block(Blocks.RED_CONCRETE, 1d, 5d, 1d),
+                                block(Blocks.RED_CONCRETE, 0d, 4d, 1d)
+                        )
+                )
+        );
+        Registration.GROWABLE_FLOWERS.add(
+                new GrowableFlower(
+                        Blocks.BLUE_ORCHID,
+                        List.of(
+                                block(Blocks.LIME_CONCRETE, 0d, 0d, 0d),
+                                block(Blocks.LIME_CONCRETE, 0d, 1d, 0d),
+                                block(Blocks.LIME_CONCRETE, 0d, 2d, 0d),
+                                block(Blocks.LIME_CONCRETE, 0d, 3d, 0d),
+                                block(Blocks.LIME_CONCRETE, 0d, 4d, 0d),
+                                block(Blocks.LIME_CONCRETE, 0d, 0d, 0d),
+                                block(Blocks.LIME_CONCRETE, 0d, 0d, -1d),
+                                block(Blocks.LIME_CONCRETE, -1d, 1d, -1d),
+                                block(Blocks.LIME_CONCRETE, -1d, 1d, 1d),
+                                block(Blocks.LIME_CONCRETE, 1d, 1d, 0d),
+                                block(Blocks.LIME_CONCRETE, 2d, 2d, 0d),
+                                block(Blocks.LIME_CONCRETE, -2d, 2d, 2d),
+                                block(Blocks.LIME_CONCRETE, -2d, 2d, -2d),
+                                block(Blocks.LIME_CONCRETE, 0d, 0d, 0d),
+                                block(Blocks.LIME_CONCRETE, 0d, 4d, 1d),
+                                block(Blocks.LIME_CONCRETE, 0d, 4d, -1d),
+                                block(Blocks.LIME_CONCRETE, 1d, 4d, -2d),
+                                block(Blocks.LIME_CONCRETE, 1d, 5d, -2d),
+                                block(Blocks.LIGHT_BLUE_CONCRETE, -2d, 3d, 2d),
+                                block(Blocks.LIGHT_BLUE_CONCRETE, -2d, 3d, -2d),
+                                block(Blocks.LIGHT_BLUE_CONCRETE, -2d, 2d, -1d),
+                                block(Blocks.LIGHT_BLUE_CONCRETE, 2d, 2d, 1d),
+                                block(Blocks.LIGHT_BLUE_CONCRETE, 2d, 3d, 0d),
+                                block(Blocks.LIGHT_BLUE_CONCRETE, 0d, 5d, 1d),
+                                block(Blocks.LIGHT_BLUE_CONCRETE, 0d, 5d, 2d),
+                                block(Blocks.LIGHT_BLUE_CONCRETE, 0d, 6d, 2d),
+                                block(Blocks.LIGHT_BLUE_CONCRETE, 1d, 5d, -1d),
+                                block(Blocks.LIGHT_BLUE_CONCRETE, 1d, 6d, -2d),
+                                block(Blocks.LIGHT_BLUE_CONCRETE, 2d, 5d, -2d)
                         )
                 )
         );
@@ -32,5 +124,9 @@ public class GrowableFlowerRegistry {
             }
         }
         return null;
+    }
+
+    private static Pair<BlockState, Vector3d> block(Block block, double x, double y, double z){
+        return new Pair<>(block.defaultBlockState(), new Vector3d(x, y, z));
     }
 }
